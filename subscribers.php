37b30c900fe9ec5cfd8736888e6d7bc064e42398
Ilya <irexont@gmail.com>
2019-11-23 16:41:06 +0300

    Fix sub_type

--- subscribers.php
+++ subscribers.php
@@ -42,7 +42,7 @@ if($act == 'options')
                 'desc' => 'Из Senler',    	
                 'default' => '',
                 'show' => [
-                    'sub_type' => [1,2,3],
+                    'sub_type' => [1,2,3,4],
                 ]       	
             ],
             'senler_utm_id' => [
