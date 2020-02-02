21180a3fb394f7d7f8be374151d4de3bb7803a8d
Ilya <irexont@gmail.com>
2020-02-02 09:07:02 +0300

    Add Catalog Cookies
    Добавил каталог для кукис

--- amo/amo_class.php
+++ amo/amo_class.php
@@ -40,8 +40,8 @@ class Amo
 	    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($user));
 	    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 	    curl_setopt($curl, CURLOPT_HEADER, false);
-	    curl_setopt($curl, CURLOPT_COOKIEFILE, $session_id.'cookie.txt'); 
-	    curl_setopt($curl, CURLOPT_COOKIEJAR, $session_id.'cookie.txt'); 
+	    curl_setopt($curl, CURLOPT_COOKIEFILE, 'cookies/'.$session_id.'cookie.txt'); 
+	    curl_setopt($curl, CURLOPT_COOKIEJAR, 'cookies/'.$session_id.'cookie.txt'); 
 	    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 	    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
 	    
@@ -66,8 +66,8 @@ class Amo
 	    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
 	    curl_setopt($curl, CURLOPT_URL, $link);
 	    curl_setopt($curl, CURLOPT_HEADER,false);
-	    curl_setopt($curl,CURLOPT_COOKIEFILE, $session_id."cookie.txt");
-	    curl_setopt($curl,CURLOPT_COOKIEJAR, $session_id."cookie.txt");
+	    curl_setopt($curl,CURLOPT_COOKIEFILE, 'cookies/'.$session_id."cookie.txt");
+	    curl_setopt($curl,CURLOPT_COOKIEJAR, 'cookies/'.$session_id."cookie.txt");
 	    $out = curl_exec($curl);
 	    curl_close($curl);
 	    $result = json_decode($out,TRUE);
@@ -97,8 +97,8 @@ class Amo
 	    curl_setopt($curl, CURLOPT_USERAGENT, 'activeusers');
 	    curl_setopt($curl, CURLOPT_URL, $link);
 	    curl_setopt($curl, CURLOPT_HEADER, false);
-	    curl_setopt($curl, CURLOPT_COOKIEFILE, $session_id.'cookie.txt'); 
-	    curl_setopt($curl, CURLOPT_COOKIEJAR, $session_id.'cookie.txt'); 
+	    curl_setopt($curl, CURLOPT_COOKIEFILE, 'cookies/'.$session_id.'cookie.txt'); 
+	    curl_setopt($curl, CURLOPT_COOKIEJAR, 'cookies/'.$session_id.'cookie.txt'); 
 	    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 	    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
 	    $out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
